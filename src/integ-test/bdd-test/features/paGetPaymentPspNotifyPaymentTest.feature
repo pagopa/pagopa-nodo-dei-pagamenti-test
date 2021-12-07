7e117a9d10740693d9626440ef8a792169d696c6
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-12-07 18:11:06 +0100

    wip

--- src/integ-test/bdd-test/features/paGetPaymentPspNotifyPaymentTest.feature
+++ src/integ-test/bdd-test/features/paGetPaymentPspNotifyPaymentTest.feature
@@ -12,10 +12,10 @@ Feature: verify test flow paGetPayment and pspNotifyPayment
                <idBrokerPSP>70000000001</idBrokerPSP>
                <idChannel>70000000001_01</idChannel>
                <password>pwdpwdpwd</password>
-               <idempotencyKey>70000000001_7632477451</idempotencyKey>
+               <idempotencyKey>70000000001_7632477483</idempotencyKey>
                <qrCode>
                   <fiscalCode>77777777777</fiscalCode>
-                  <noticeNumber>302114719472095720</noticeNumber>
+                  <noticeNumber>302114719472095752</noticeNumber>
                </qrCode>
                <amount>120.00</amount>
             </nod:activateIOPaymentReq>
@@ -30,14 +30,19 @@ Feature: verify test flow paGetPayment and pspNotifyPayment
     And token exists and check
 
   # Payment Phase
-#  Scenario: Execute nodoChiediInformazioniPagamento request
-#    Given the activate phase executed successfully
-#    When WISP/PM sends an informazioniPagamento to nodo-dei-pagamenti using the token of the activate phase
-#    Then verify the HTTP status code response is 200
+  Scenario: Execute nodoChiediInformazioniPagamento request
+    Given the activate phase executed successfully
+    When WISP/PM sends an informazioniPagamento to nodo-dei-pagamenti using the token of the activate phase
+    Then verify the HTTP status code response is 200
 
   # Send payment to PSP Phase
-#  Scenario: Execute nodoInoltraEsitoPagamentoCarta request
-#    Given the payment phase executed successfully
-#    When WISP/PM sends an inoltroEsito/Carta to nodo-dei-pagamenti using the token and PSP/Canale data
-#    Then verify the HTTP status code response is 200
-#    And activeIOPaymentResp and pspNotifyPaymentReq are consistent
+  Scenario: Execute nodoInoltraEsitoPagamentoCarta request
+    Given the payment phase executed successfully
+    When WISP/PM sends an inoltroEsito/Carta to nodo-dei-pagamenti using the token and PSP/Canale data
+    Then verify esito is OK
+
+  # Verify PA response is coherent with PSP Request
+  Scenario: Verify consistency between activateIOPaymentRes and pspNotifyPaymentReq
+    Given the payment notify phase executed successfully
+    Then activateIOPaymentResp and pspNotifyPaymentReq are consistent
+
--- src/integ-test/bdd-test/features/steps/steps.py
+++ src/integ-test/bdd-test/features/steps/steps.py
@@ -1,11 +1,22 @@
-from xml.dom.minidom import parseString
-import requests
+from xml.dom.minidom import parseString, parse
+import requests, json
 from behave import *
+import xmltodict
+
+def get_soap_url_nodo(context):
+    if context.config.userdata.get("services").get("nodo-dei-pagamenti").get("soap_service") is not None:
+        return context.config.userdata.get("services").get("nodo-dei-pagamenti").get("url") \
+           + context.config.userdata.get("services").get("nodo-dei-pagamenti").get("soap_service")
+    else:
+        return ""
 
 
-def get_url_nodo(context):
-    return context.config.userdata.get("services").get("nodo-dei-pagamenti").get("url") \
-           + context.config.userdata.get("services").get("nodo-dei-pagamenti").get("service")
+def get_rest_url_nodo(context):
+    if context.config.userdata.get("services").get("nodo-dei-pagamenti").get("rest_service") is not None:
+        return context.config.userdata.get("services").get("nodo-dei-pagamenti").get("url") \
+           + context.config.userdata.get("services").get("nodo-dei-pagamenti").get("rest_service")
+    else:
+        return ""
 
 
 # Background
@@ -63,7 +74,7 @@ def step_impl(context, attribute, value, elem):
 @when('psp sends verifyPaymentNoticeReq to nodo-dei-pagamenti')
 def step_impl(context):
     headers = {'Content-Type': 'application/xml'}  # set what your server accepts
-    url_nodo = get_url_nodo(context)
+    url_nodo = get_soap_url_nodo(context)
     nodo_response = requests.post(url_nodo, context.soap_request, headers=headers)
     assert (nodo_response.status_code == 200)
 
@@ -72,7 +83,7 @@ def step_impl(context):
 @then('check {tag} is {value}')
 def step_impl(context, tag, value):
     headers = {'Content-Type': 'application/xml'}  # set what your server accepts
-    url_nodo = get_url_nodo(context)
+    url_nodo = get_soap_url_nodo(context)
     nodo_response = requests.post(url_nodo, context.soap_request, headers=headers)
     my_document = parseString(nodo_response.content)
     if len(my_document.getElementsByTagName('faultCode')) > 0:
@@ -85,7 +96,7 @@ def step_impl(context, tag, value):
 @when("IO sends an activateIOPaymentReq to nodo-dei-pagamenti")
 def step_impl(context):
     headers = {'Content-Type': 'application/xml'}  # set what your server accepts
-    url_nodo = get_url_nodo(context)
+    url_nodo = get_soap_url_nodo(context)
     nodo_response = requests.post(url_nodo, context.soap_request, headers=headers)
     setattr(context, "soap_response", nodo_response)
     assert nodo_response.status_code == 200
@@ -96,56 +107,123 @@ def step_impl(context):
     my_document = parseString(context.soap_response.content)
     if len(my_document.getElementsByTagName('paymentToken')) > 0:
         paymentToken = my_document.getElementsByTagName('paymentToken')[0].firstChild.data
-        setattr(context, "paymentToken", paymentToken)
+        test_configuration = context.config.userdata.get("test_configuration")
+        if test_configuration is None:
+            test_configuration = {}
+        test_configuration["paymentToken"] = paymentToken
+        test_configuration["payment_phase"] = True
+        context.config.update_userdata({"test_configuration": test_configuration})
+        # setattr(context, "paymentToken", paymentToken)
         assert 0 < len(paymentToken) < 36
     else:
         assert False
 
 
-
-@then('verify the HTTP {staus_code} response is {value}')
-def step_impl(context, tag, value):
-    pass
-
 # Scenario: Execute nodoChiediInformazioniPagamento request
 @given("the activate phase executed successfully")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Given the activate phase executed successfully')
+    # TODO investigate how to use the following steps
+    # context.execute_steps(''' When IO sends an activateIOPaymentReq to nodo-dei-pagamenti ''')
+    # context.execute_steps(''' Then token exists and check ''')
+    assert "paymentToken" in context.config.userdata.get("test_configuration")
 
 
 @when("WISP/PM sends an informazioniPagamento to nodo-dei-pagamenti using the token of the activate phase")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(
-        u'STEP: When WISP/PM sends an informazioniPagamento to nodo-dei-pagamenti using the token of the activate phase')
+    url_nodo = get_rest_url_nodo(context)
+    paymentToken = context.config.userdata.get("test_configuration").get("paymentToken")
+    headers = {'Content-Type': 'application/json'}
+    nodo_response = requests.get(f"{url_nodo}/informazioniPagamento?idPagamento={paymentToken}", headers=headers)
+    assert nodo_response.status_code == 200
+
+
+@then('verify the HTTP {status_code} response is {value}')
+def step_impl(context, status_code, value):
+    # at the moment, nothing to check
+    assert True
 
 
 # Scenario: Execute nodoInoltraEsitoPagamentoCarta request
 @given("the payment phase executed successfully")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Given the payment phase executed successfully')
+    assert "payment_phase" in context.config.userdata.get("test_configuration")
 
 
 @when("WISP/PM sends an inoltroEsito/Carta to nodo-dei-pagamenti using the token and PSP/Canale data")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(
-        u'STEP: When WISP/PM sends an inoltroEsito/Carta to nodo-dei-pagamenti using the token and PSP/Canale data')
+    print("")
+    url_nodo = get_rest_url_nodo(context)
+    paymentToken = context.config.userdata.get("test_configuration").get("paymentToken")
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "idPagamento": paymentToken,
+        "RRN": 0,
+        "identificativoPsp": "40000000001",
+        "tipoVersamento": "CP",
+        "identificativoIntermediario": "40000000001",
+        "identificativoCanale": "40000000001_06",
+        "importoTotalePagato": 20.1,
+        "timestampOperazione": "2012-04-23T18:25:43Z",
+        "codiceAutorizzativo": "666666",
+        "esitoTransazioneCarta": "ok"
+    }
+    nodo_response = requests.post(f"{url_nodo}/inoltroEsito/carta", json=body, headers=headers)
+    setattr(context, "rest_response", nodo_response)
+    test_configuration = context.config.userdata.get("test_configuration")
+    test_configuration["payment_notify_phase"] = True
+    context.config.update_userdata(test_configuration)
+    assert nodo_response.status_code == 200
 
 
-@step("activeIOPaymentResp and pspNotifyPaymentReq are consistent")
+@then("verify esito is OK")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: And activeIOPaymentResp and pspNotifyPaymentReq are consistent')
+    node_response = getattr(context, "rest_response")
+    json_response = node_response.json()
+    assert json_response.get("esito") == "OK"
+
+
+# Scenario: Verify consistency between activateIOPaymentResp and pspNotifyPaymentReq
+@given("the payment notify phase executed successfully")
+def step_impl(context):
+    assert "payment_notify_phase" in context.config.userdata.get("test_configuration")
+
+
+@then("activateIOPaymentResp and pspNotifyPaymentReq are consistent")
+def step_impl(context):
+    # TODO retrieve resource from mock
+    # url_nodo = get_rest_url_nodo()
+    # nodo_response = requests.get(f"{url_nodo}/inoltroEsito/carta", json=body, headers=headers)
+
+    paGetPaymentResXML = open("/tmp/paGetPaymentRes.xml", 'r')
+    paGetPaymentRes = xmltodict.parse(paGetPaymentResXML.read())
+    pspNotifyPaymentReqJson = open("/tmp/pspNotifyPaymentReq.json", 'r')
+    pspNotifyPaymentReq = json.loads(pspNotifyPaymentReqJson.read())
+
+    paGetPaymentRes_transferList = paGetPaymentRes["soapenv:Envelope"]["soapenv:Body"]["paf:paGetPaymentRes"]["data"]["transferList"]["transfer"]
+    pspNotifyPaymentReq_transferList = pspNotifyPaymentReq.get("soapenv:envelope").get("soapenv:body")[0].get("pspfn:pspnotifypaymentreq")[0].get("transferlist")
+
+    # verify transfer list are equal
+    verified = True
+    for paTransfer in paGetPaymentRes_transferList:
+        found = False
+        for pspTransfer in pspNotifyPaymentReq_transferList:
+            if paTransfer.get("idTransfer") == pspTransfer.get("transfer").get("idTransfer")[0] and \
+                    paTransfer.get("transferAmount") == pspTransfer.get("transfer").get("transferAmount")[0] and \
+                    paTransfer.get("fiscalCode") == pspTransfer.get("transfer").get("fiscalCode")[0] and \
+                    paTransfer.get("IBAN") == pspTransfer.get("transfer").get("IBAN")[0] and \
+                    paTransfer.get("remittanceInformation") == pspTransfer.get("transfer").get("remittanceInformation")[0] and \
+                    paTransfer.get("transferCategory") == pspTransfer.get("transfer").get("transferCategory")[0]:
+                found = True
+        if not found:
+            verified = False
+            break
+
+    assert verified
+
+
+
+
+
+
+
+
