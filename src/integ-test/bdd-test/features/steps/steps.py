e588fd35e874b9a72f3591b6dec029892a0474c3
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-12-27 16:34:08 +0100

    improvements

--- src/integ-test/bdd-test/features/steps/steps.py
+++ src/integ-test/bdd-test/features/steps/steps.py
@@ -64,7 +64,7 @@ def step_impl(context, type_soap_request):
 
 @given('{elem} with {value} in {action}')
 def step_impl(context, elem, value, action):
-    if action == "verifyPaymentNoticeReq" or action == "activatePaymentNoticeReq" :
+    if action == "verifyPaymentNoticeReq" or action == "activatePaymentNoticeReq" or action == "verificaBollettinoReq":
         xml = utils.manipulate_soap_action(context.soap_request, elem, value)
         setattr(context, "soap_request", xml)
     elif action == "paVerifyPaymentNoticeRes":
