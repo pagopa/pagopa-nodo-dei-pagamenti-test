44c1f0495c400b8f85898ac2d96ec8f848100f5d
nicolo-isaia <104012607+nicolo-isaia@users.noreply.github.com>
2022-07-18 12:34:12 +0200

    fix

--- src/integ-test/bdd-test/features/steps/steps.py
+++ src/integ-test/bdd-test/features/steps/steps.py
@@ -63,7 +63,7 @@ def step_impl(context, primitive):
         payload = payload.replace('#idempotency_key#', f"{idBrokerPSP}_{str(random.randint(1000000000, 9999999999))}")
 
     if '#notice_number#' in payload:
-        notice_number = f"31111{str(random.randint(1000000000000, 9999999999999))}"
+        notice_number = f"30211{str(random.randint(1000000000000, 9999999999999))}"
         payload = payload.replace('#notice_number#', notice_number)
         setattr(context, "iuv", notice_number[1:])
     
