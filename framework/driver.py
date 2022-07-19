33025c255905d27378b375b5267706c5e596773b
Sorrentino, S. B <s.b.sorrentino@accenture.com>
2022-07-19 18:27:04 +0200

    fixed

--- framework/driver.py
+++ framework/driver.py
@@ -1,5 +1,5 @@
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chromium.options import ChromiumOptions
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
