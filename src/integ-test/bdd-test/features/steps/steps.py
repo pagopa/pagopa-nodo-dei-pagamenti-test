3ec8473f0c51b231525d65cc9c3930b5d70df0d9
DamianoDor <damiano.dorelli@accenture.com>
2022-07-19 18:19:14 +0200

    add new test nm3

--- src/integ-test/bdd-test/features/steps/steps.py
+++ src/integ-test/bdd-test/features/steps/steps.py
@@ -33,7 +33,7 @@ def step_impl(context):
             - pagopa-api-config ( used in tests to set DB's nodo-dei-pagamenti correctly according to input test ))
     """
     responses = True
-    """ DA DECOMMENTARE APPENA SPICA RISOLVE MAPPING HEALTHCHECK 
+    
     for row in context.table:
         print(f"calling: {row.get('name')} -> {row.get('url')}")
         url = row.get("url") + row.get("healthcheck")
@@ -41,7 +41,7 @@ def step_impl(context):
         resp = requests.get(url)
         print(f"response: {resp.status_code}")
         responses &= (resp.status_code == 200)
-    """
+
     assert responses
 
 
@@ -676,7 +676,6 @@ def step_impl(context, query_name, macro, db_name,table_name,columns):
     if exec_query is not None:
         print(f'executed query: {exec_query}')
     setattr(context, query_name, exec_query)
-
 
 
 @step("with the query {query_name1} check assert beetwen elem {elem1} in position {position1:d} and elem {elem2} with position {position2:d} of the query {query_name2}")
