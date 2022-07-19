3f8a15349c54b2f2c8fc7d50bd0a2eecbb71d5e7
nicolo-isaia <104012607+nicolo-isaia@users.noreply.github.com>
2022-07-19 12:59:49 +0200

    fix

--- framework/driver.py
+++ framework/driver.py
@@ -19,7 +19,7 @@ class Driver:
             case "chrome":
                 print('init driverrrr')
                 options = webdriver.ChromeOptions()
-                #options.add_argument('--headless')
+                options.add_argument('--headless')
                 options.add_argument('no-sandbox')
                 options.add_argument('--disable-gpu')
                 options.add_argument('--disable-dev-shm-usage')
