340f6115682d18abc6e433e1b45cc0c5c6433ae0
Sorrentino, S. B <s.b.sorrentino@accenture.com>
2022-07-20 08:43:44 +0200

    fix

--- framework/driver.py
+++ framework/driver.py
@@ -19,12 +19,12 @@ class Driver:
                 print('init driverrrr')
                 options = Options()
                 options.binary_location = '/usr/local/bin/chromedriver'
-                options.add_argument('--no-sandbox')
+                options.add_argument('no-sandbox')
                 options.add_argument('--headless')
                 #options.add_argument('--disable-gpu') Only for Windows
                 options.add_argument('--disable-dev-shm-usage')
                 #options.add_argument('--start-fullscreen')
-                self.driver = webdriver.Chrome(options=options, executable_path='/usr/local/bin/chromedriver')
+                self.driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/local/bin/chromedriver')
                 #sleep(10)
         elif "firefox":
             pass
