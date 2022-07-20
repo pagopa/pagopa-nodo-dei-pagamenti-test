10544e6f1572950a0ef016ad8ff336525283a559
nicolo-isaia <104012607+nicolo-isaia@users.noreply.github.com>
2022-07-20 16:46:43 +0200

    fix

--- features/environment.py
+++ features/environment.py
@@ -2,9 +2,9 @@ import cx_Oracle, os, sys
 
 def before_all(context):
 
-    #lib_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, 'instantclient_21_6'))
-    #cx_Oracle.init_oracle_client(lib_dir = lib_dir)
-    pass
+    lib_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, 'instantclient_21_6'))
+    cx_Oracle.init_oracle_client(lib_dir = lib_dir)
+    #pass
 
 def before_feature(context,feature):
     #print('before feature')
