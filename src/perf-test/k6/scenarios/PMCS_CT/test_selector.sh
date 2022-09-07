9ba386458e4d93ca159aa90bf8935d131e296f6c
alfonso.iazzetti <alfonso.iazzetti@host.docker.internal>
2022-09-07 16:13:58 +0200

    updated test_selector script
    Signed-off-by: alfonso.iazzetti <alfonso.iazzetti@host.docker.internal>

--- src/perf-test/k6/scenarios/PMCS_CT/test_selector.sh
+++ src/perf-test/k6/scenarios/PMCS_CT/test_selector.sh
@@ -17,7 +17,7 @@ echo PROG:   $progDir
 echo calling 'k6 run -e scenario='$scenario' -e test='$test' -e steps='$steps' -e env='$env '"'$progDir'/scenarios/'$scenario'/test/'$test'.js"' command...
 
 # k6 run --out csv='./scenarios/'$scenario'/test/output/'$test'.csv' -e scenario=$scenario -e test=$test -e steps=$steps -e env=$env './scenarios/'$scenario'/test/'$test'.js'
-./k6 run --out xk6-influxdb=http://localhost:8086/k6database -e scenario=$scenario -e test=$test -e steps=$steps -e env=$env './scenarios/'$scenario'/test/'$test'.js'
+k6 run --out xk6-influxdb=http://localhost:8086/k6database -e scenario=$scenario -e test=$test -e steps=$steps -e env=$env './scenarios/'$scenario'/test/'$test'.js'
 #k6 run -e scenario=$scenario -e test=$test -e steps=$steps -e env=$env './scenarios/'$scenario'/test/'$test'.js' --out influxdb=http://127.0.0.1:8086/k6database
 #./k6 run --out csv='.\scenarios\'$scenario'\test\output\'$test'.csv' -e scenario=$scenario -e test=$test -e steps=$steps -e env=$env '.\scenarios\'$scenario'\test\'$test'.js'
 
