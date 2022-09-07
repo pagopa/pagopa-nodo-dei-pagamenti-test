b960ce8fc76ffd961c58ff0f0ee4f6b32b41eb36
alessandrocancelli93 <104897108+alessandrocancelli93@users.noreply.github.com>
2022-09-07 09:15:28 +0200

    fix

--- src/integ-test/bdd-test/features/environment.py
+++ src/integ-test/bdd-test/features/environment.py
@@ -11,9 +11,9 @@ import steps.utils as utils
 
 def before_all(context):
     print('Global settings...')
-    lib_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, "instantclient_21_6"))
+    lib_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, 'oracle', 'instantclient_21_6'))
     cx_Oracle.init_oracle_client(lib_dir=lib_dir)
-    more_userdata = json.load(open(os.path.join(context.config.base_dir + "/../resources/config.json")))
+    more_userdata = json.load(open(os.path.join(context.config.base_dir + "/../resources/pipeline_config.json")))
     context.config.update_userdata(more_userdata)
     #services = context.config.userdata.get("services")
     #db_config = context.config.userdata.get("db_configuration")
