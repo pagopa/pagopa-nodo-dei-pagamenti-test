468b1a4dfebc9f0bb163b986c2833ffae6f27657
alfonso.iazzetti <alfonso.iazzetti@host.docker.internal>
2022-09-09 12:01:49 +0200

    added js, skip all PMCS script except TC01.02
    Signed-off-by: alfonso.iazzetti <alfonso.iazzetti@host.docker.internal>

---
+++ src/perf-test/k6/scenarios/CommonScript.js
@@ -0,0 +1,18 @@
+export function handleSummary(data, path) {
+  console.log('Preparing the end-of-test summary...');
+  console.log('out path is '+ path);
+ 
+  var csv = outputUtil.extractData(data);
+  let d = (new Date).toISOString().substr(0,10);
+     
+   return {
+    'stdout': textSummary(data, { indent: ' ', enableColors: true, expected_response: 'ALL' }), // Show the text summary to stdout...
+	[`${path}/${d}_TC01.02.summary.json`]: JSON.stringify(data), // and a JSON with all the details...
+	//${path}/summary.html': htmlReport(data),
+	[`${path}/${d}_TC01.02.summary.csv`]: csv[0],
+	[`${path}/${d}_TC01.02.trOverSla.csv`]: csv[1],
+	[`${path}/${d}_TC01.02.resultCodeSummary.csv`]: csv[2],
+	 	
+  };
+  
+}
