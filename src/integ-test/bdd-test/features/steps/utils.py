f48ae282df16255b53267b69e381de626359d71a
DamianoDor <damiano.dorelli@accenture.com>
2022-10-12 17:19:01 +0200

    fix utils

--- src/integ-test/bdd-test/features/steps/utils.py
+++ src/integ-test/bdd-test/features/steps/utils.py
@@ -255,13 +255,14 @@ def single_thread(context, soap_primitive, type):
     primitive = replace_global_variables(primitive, context)
     print(soap_primitive.split("_")[1])
     headers = {'Content-Type': 'application/xml', 'SOAPAction': primitive, 'X-Forwarded-For': '10.82.39.148', 'Host': 'api.dev.platform.pagopa.it:443'}
-    url_nodo = get_soap_url_nodo(context, primitive) if 'xml' in getattr(context, primitive) else f"{get_rest_url_nodo(context)}/{primitive}"
     print(url_nodo)
     print("nodo soap_request sent >>>", getattr(
         context, soap_primitive.split("_")[1]))
     if type == 'GET':
+        url_nodo = f"{get_rest_url_nodo(context)}/{primitive}"
         soap_response = requests.get(url_nodo, headers=headers, verify=False)
     elif type == 'POST':
+        url_nodo = get_soap_url_nodo(context, primitive)
         soap_response = requests.post(url_nodo, getattr(
             context, soap_primitive.split("_")[1]), headers=headers, verify=False)
     print("nodo soap_response: ", soap_response.content)
