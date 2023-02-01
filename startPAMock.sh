8bcd21a48b90bb91eb94071dab66a1bead5e0345
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-02-01 17:48:52 +0100

    mock prf helm

--- startPAMock.sh
+++ startPAMock.sh
@@ -1,3 +1,1 @@
-mv PA_Mock-*.jar PA_MOCK.jar
-
-java -Xmx500m -Dlog4j.configurationFile=log4j2.xml -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar PA_MOCK.jar --address=http://0.0.0.0:8080/servizio
+java -Xmx500m -Dlog4j.configurationFile=log4j2.xml -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar PA_Mock-1.3.0.jar --address=http://0.0.0.0:8080/servizio
