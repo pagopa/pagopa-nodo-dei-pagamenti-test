7aba2416c3287553a719e68567ffd65f80eb20e0
lorenzo1046 <lorenzo.francia46@gmail.com>
2023-05-05 12:02:19 +0200

    fix utils for parallel calls

--- src/integ-test/bdd-test/features/steps/utils.py
+++ src/integ-test/bdd-test/features/steps/utils.py
@@ -320,7 +320,7 @@ def single_thread(context, soap_primitive, type):
     
     if type == 'GET':
         
-        # headers = {'X-Forwarded-For': '10.82.39.148', 'Host': 'api.dev.platform.pagopa.it:443'}
+        headers = {'X-Forwarded-For': '10.82.39.148', 'Host': 'api.dev.platform.pagopa.it:443'}
         if 'SUBSCRIPTION_KEY' in os.environ:
             headers = {'Ocp-Apim-Subscription-Key', os.getenv('SUBSCRIPTION_KEY') }
         url_nodo = f"{get_rest_url_nodo(context)}/{primitive}"
