df1070eb654109df3079c064d06c1f3b67feb8e1
Collini <alex.collini@accenture.com>
2023-10-27 17:03:48 +0200

    Add debug echo for PID

--- startIntTest.sh
+++ startIntTest.sh
@@ -99,7 +99,8 @@ ps -aux
 
 if [ "$fromAPI" = true ]; then
     echo "Restarting allure..."
-    kill $(ps aux | grep '[j]ava' | awk '{print $2}')
+    echo $(ps aux | grep '[j]ava' | awk '{print $2}')
+    kill -9 $(ps aux | grep '[j]ava' | awk '{print $2}')
 fi
 
 allure serve /test/allure -p 8081
