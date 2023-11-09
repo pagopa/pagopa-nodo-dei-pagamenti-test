e19d7f1813a826c69f256a34ba8175183e23e628
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2023-11-09 16:21:10 +0100

    fix argument

--- stopIntTest.sh
+++ stopIntTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ps -aef | grep startIntTest | grep -v grep | awk '{ print $1 }' | xargs kill -9 & \
-ps -aef | grep behave | grep -v grep | awk '{ print $1 }' | xargs kill -9 & \
+ps -aef | grep startIntTest | grep -v grep | awk '{ print $2 }' | xargs kill -9 & \
+ps -aef | grep behave | grep -v grep | awk '{ print $2 }' | xargs kill -9 & \
 ps -aef
