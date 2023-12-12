d0668036183945bb581704f6b57444d689cabeef
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2023-12-12 16:45:02 +0100

    added log api

--- manualtrigger.py
+++ manualtrigger.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+from zipfile import ZipFile
+
 import os
 import threading
 import subprocess
 app = Flask(__name__)
+
+logs_directory = 'test/allure/allure-result'
+zip_file_path = 'test/allure/allure-result.zip'
 
 def run_script(tags, folder):
     # execute script
@@ -35,6 +40,20 @@ def stop_test():
 	script_thread = threading.Thread(target=run_stop_script)
 	script_thread.start()
 	return jsonify({"message": "test stopped"})
+
+def create_zip_file():
+    with ZipFile(zip_file_path, 'w') as zipf:
+        for root, _, files in os.walk(logs_directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, logs_directory)
+                zipf.write(file_path, arcname=arcname)
+                
+@app.route('/log', methods=['GET'])
+def get_logs():
+	if not os.path.exists(zip_file_path):
+		create_zip_file()
+	return send_file(zip_file_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8082)
