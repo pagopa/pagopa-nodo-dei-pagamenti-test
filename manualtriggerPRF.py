0eb79fed6b218f993e5c305fbe9957f88cc3ba3b
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2024-02-07 15:10:00 +0100

    added log

--- manualtriggerPRF.py
+++ manualtriggerPRF.py
@@ -6,15 +6,17 @@ app = Flask(__name__)
 
 def run_script(debugEnabled, rampa, blacklist):
     # execute script
+    print('starting startPerfTest')
     subprocess.Popen(["bash", "./startPerfTest.sh", debugEnabled, rampa, blacklist], stdin=subprocess.PIPE)
 
 def run_stop_script():
     # execute script
+    print('stopping test...')
     subprocess.Popen(["bash", "./stopPerfTest.sh"], stdin=subprocess.PIPE)
 	
 @app.route('/starttest', methods=['POST'])
 def start_test():
-    
+    print('start test START')
 	#read body
 	data = request.get_json()
 
@@ -26,15 +28,18 @@ def start_test():
 	    # execute tests
 		script_thread = threading.Thread(target=run_script, args=(debugEnabled, rampa, blacklist))
 		script_thread.start()
-	
+		print('start test END')
 		return jsonify({"message": "test started"})
 	else :
+		print('start test error START')
 		return jsonify({"error": "missing required keys"}), 400
 
 @app.route('/stoptest', methods=['GET'])
 def stop_test():
+	print('stop test START')
 	script_thread = threading.Thread(target=run_stop_script)
 	script_thread.start()
+	print('stop test END')
 	return jsonify({"message": "test stopped"})
 	
 if __name__ == '__main__':
