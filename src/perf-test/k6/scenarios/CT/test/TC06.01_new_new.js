dc62ae22ab5f1458403f5eeed6e61bd0761c2add
lorenzo1046 <lorenzo.francia@accenture.com>
2024-02-09 10:46:47 +0100

    add new scenario TC06.01 new new

---
+++ src/perf-test/k6/scenarios/CT/test/TC06.01_new_new.js
@@ -0,0 +1,156 @@
+import { check } from 'k6';
+//import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { SharedArray } from 'k6/data';
+import papaparse from './util/papaparse.js';
+import { checkPosition } from './api/checkPosition.js';
+import * as common from '../../CommonScript.js';
+import * as inputDataUtil from './util/input_data_util.js';
+
+
+const csvBaseUrl = new SharedArray('baseUrl', function () {
+
+    return papaparse.parse(open('../../../cfg/baseURL_Nodo.csv'), { header: true }).data;
+
+});
+
+
+const chars = '0123456789';
+// NoticeNumber
+export function genNoticeNumber() {
+    let noticeNumber = '311';
+    for (var i = 15; i > 0; --i) noticeNumber += chars[Math.floor(Math.random() * chars.length)];
+    return noticeNumber;
+}
+
+
+export const getScalini = new SharedArray('scalini', function () {
+
+
+    const f = JSON.parse(open('../../../cfg/' + `${__ENV.steps}` + '.json'));
+
+    return f;
+});
+
+export const options = {
+
+    scenarios: {
+        total: {
+            timeUnit: '4s',
+            preAllocatedVUs: 1, // how large the initial pool of VUs would be
+            executor: 'ramping-arrival-rate',
+            //executor: 'ramping-vus',
+            maxVUs: 500,
+            stages: [
+                { target: getScalini[0].Scalino_CT_1, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_1, duration: getScalini[0].Scalino_CT_TIME_1 + 's' },
+                { target: getScalini[0].Scalino_CT_2, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_2, duration: getScalini[0].Scalino_CT_TIME_2 + 's' },
+                { target: getScalini[0].Scalino_CT_3, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_3, duration: getScalini[0].Scalino_CT_TIME_3 + 's' },
+                { target: getScalini[0].Scalino_CT_4, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_4, duration: getScalini[0].Scalino_CT_TIME_4 + 's' },
+                { target: getScalini[0].Scalino_CT_5, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_5, duration: getScalini[0].Scalino_CT_TIME_5 + 's' },
+                { target: getScalini[0].Scalino_CT_6, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_6, duration: getScalini[0].Scalino_CT_TIME_6 + 's' },
+                { target: getScalini[0].Scalino_CT_7, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_7, duration: getScalini[0].Scalino_CT_TIME_7 + 's' },
+                { target: getScalini[0].Scalino_CT_8, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_8, duration: getScalini[0].Scalino_CT_TIME_8 + 's' },
+                { target: getScalini[0].Scalino_CT_9, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_9, duration: getScalini[0].Scalino_CT_TIME_9 + 's' },
+                { target: getScalini[0].Scalino_CT_10, duration: 0 + 's' },
+                { target: getScalini[0].Scalino_CT_10, duration: getScalini[0].Scalino_CT_TIME_10 + 's' }, //to uncomment
+            ],
+            tags: { test_type: 'ALL' },
+            exec: 'total',
+        }
+
+    },
+    summaryTrendStats: ['avg', 'min', 'max', 'p(90)', 'p(95)', 'count'],
+    discardResponseBodies: false,
+    thresholds: {
+
+        'http_req_duration{checkPosition:http_req_duration}': [],
+        'http_req_duration{closePayment:http_req_duration}': [],
+        'http_req_duration{sendPaymentOutcome:http_req_duration}': [],
+        'http_req_duration{ActivateIOPayment:http_req_duration}': [],
+        'http_req_duration{ALL:http_req_duration}': [],
+        'checks{checkPosition:over_sla300}': [],
+        'checks{checkPosition:over_sla400}': [],
+        'checks{checkPosition:over_sla500}': [],
+        'checks{checkPosition:over_sla600}': [],
+        'checks{checkPosition:over_sla800}': [],
+        'checks{checkPosition:over_sla1000}': [],
+        'checks{checkPosition:ok_rate}': [],
+        'checks{checkPosition:ko_rate}': [],
+        'checks{closePayment:over_sla300}': [],
+        'checks{closePayment:over_sla400}': [],
+        'checks{closePayment:over_sla500}': [],
+        'checks{closePayment:over_sla600}': [],
+        'checks{closePayment:over_sla800}': [],
+        'checks{closePayment:over_sla1000}': [],
+        'checks{closePayment:ok_rate}': [],
+        'checks{closePayment:ko_rate}': [],
+        'checks{sendPaymentOutcome:over_sla300}': [],
+        'checks{sendPaymentOutcome:over_sla400}': [],
+        'checks{sendPaymentOutcome:over_sla500}': [],
+        'checks{sendPaymentOutcome:over_sla600}': [],
+        'checks{sendPaymentOutcome:over_sla800}': [],
+        'checks{sendPaymentOutcome:over_sla1000}': [],
+        'checks{sendPaymentOutcome:ok_rate}': [],
+        'checks{sendPaymentOutcome:ko_rate}': [],
+        'checks{ActivateIOPayment:over_sla300}': [],
+        'checks{ActivateIOPayment:over_sla400}': [],
+        'checks{ActivateIOPayment:over_sla500}': [],
+        'checks{ActivateIOPayment:over_sla600}': [],
+        'checks{ActivateIOPayment:over_sla800}': [],
+        'checks{ActivateIOPayment:over_sla1000}': [],
+        'checks{ActivateIOPayment:ok_rate}': [],
+        'checks{ActivateIOPayment:ko_rate}': [],
+        'checks{ALL:over_sla300}': [],
+        'checks{ALL:over_sla400}': [],
+        'checks{ALL:over_sla500}': [],
+        'checks{ALL:over_sla600}': [],
+        'checks{ALL:over_sla800}': [],
+        'checks{ALL:over_sla1000}': [],
+        'checks{ALL:ok_rate}': [],
+        'checks{ALL:ko_rate}': [],
+    },
+
+
+};
+
+
+
+export function total() {
+
+    let baseSoapUrl = "";
+    let baseRestUrl = "";
+    let urls = csvBaseUrl;
+    for (var key in urls) {
+        if (urls[key].ENV == `${__ENV.env}`) {
+
+            baseSoapUrl = urls[key].SOAP_BASEURL;
+            baseRestUrl = urls[key].REST_BASEURL;
+        }
+    }
+
+    //let rndAnagPsp = inputDataUtil.getAnagPsp();
+    let rndAnagPaNew = inputDataUtil.getAnagPaNew();
+
+    let noticeNmbr = genNoticeNumber();
+    //let idempotencyKey = genIdempotencyKey();
+
+
+
+    let res = checkPosition(baseSoapUrl, rndAnagPaNew, noticeNmbr);
+
+
+    // res = sendPaymentOutput(baseSoapUrl,rndAnagPsp,paymentToken);
+
+}
+
+export default function () {
+    total();
+}
---
+++ src/perf-test/k6/scenarios/CT/test/api/checkPosition.js
@@ -0,0 +1,105 @@
+import http from 'k6/http';
+import { check, fail } from 'k6';
+import { parseHTML } from "k6/html";
+import { Trend } from 'k6/metrics';
+import { getBasePath, getHeaders } from "../util/base_path_util.js";
+
+export const checkPosition_Trend = new Trend('checkPosition');
+export const All_Trend = new Trend('ALL');
+
+export function checkPositionBody(cfpa, noticeNmbr) {
+    return `
+{
+    "positionslist": [
+        {
+            "fiscalCode": "${cfpa}",
+            "noticeNumber": "${noticeNmbr}"
+        }
+    ]
+}
+`
+};
+
+export function checkPosition(baseUrl, rndAnagPa, noticeNmbr) {
+
+    console.log(checkPositionBody(rndAnagPa.CF, noticeNmbr));
+    const res = http.post(
+        getBasePath(baseUrl, "checkPosition"),
+        checkPositionBody(rndAnagPa.CF, noticeNmbr),
+        {
+            headers: getHeaders({ 'Content-Type': 'text/xml', 'SOAPAction': 'checkPosition' }),
+            tags: { checkPosition: 'http_req_duration', ALL: 'http_req_duration', primitiva: "checkPosition" }
+        }
+    );
+
+    console.debug("checkPosition RES");
+    console.debug(JSON.stringify(res));
+
+    checkPosition_Trend.add(res.timings.duration);
+    All_Trend.add(res.timings.duration);
+
+    check(res, {
+        'checkPosition:over_sla300': (r) => r.timings.duration > 300,
+    },
+        { checkPosition: 'over_sla300', ALL: 'over_sla300' }
+    );
+
+    check(res, {
+        'checkPosition:over_sla400': (r) => r.timings.duration > 400,
+    },
+        { checkPosition: 'over_sla400', ALL: 'over_sla400' }
+    );
+
+    check(res, {
+        'checkPosition:over_sla500 ': (r) => r.timings.duration > 500,
+    },
+        { checkPosition: 'over_sla500', ALL: 'over_sla500' }
+    );
+
+    check(res, {
+        'checkPosition:over_sla600': (r) => r.timings.duration > 600,
+    },
+        { checkPosition: 'over_sla600', ALL: 'over_sla600' }
+    );
+
+    check(res, {
+        'checkPosition:over_sla800': (r) => r.timings.duration > 800,
+    },
+        { checkPosition: 'over_sla800', ALL: 'over_sla800' }
+    );
+
+    check(res, {
+        'checkPosition:over_sla1000': (r) => r.timings.duration > 1000,
+    },
+        { checkPosition: 'over_sla1000', ALL: 'over_sla1000' }
+    );
+
+    let outcome = '';
+    try {
+        const doc = parseHTML(res.body);
+        const script = doc.find('outcome');
+        outcome = script.text();
+    } catch (error) { }
+
+    check(
+        res,
+        {
+            'checkPosition:ok_rate': (r) => outcome == 'OK',
+        },
+        { checkPosition: 'ok_rate', ALL: 'ok_rate' }
+    );
+
+    if (check(
+        res,
+        {
+            'checkPosition:ko_rate': (r) => outcome !== 'OK',
+        },
+        { checkPosition: 'ko_rate', ALL: 'ko_rate' }
+    )) {
+        fail("outcome != ok: " + outcome);
+    }
+
+    return res;
+
+}
+
