81c5cd31e26ae41e27b8a166efb58487ad37c445
Lucaaccenture <luca.acone@accenture.com>
2024-02-13 17:12:09 +0100

    upd print

--- src/integ-test/bdd-test/features/steps/db_operation_apicfg_testing_support.py
+++ src/integ-test/bdd-test/features/steps/db_operation_apicfg_testing_support.py
@@ -38,7 +38,7 @@ def executeQuery(conn, query:str) -> list:
         if 'APICFG_SUBSCRIPTION_KEY' in os.environ:
             print("#############APICFG_SUBSCRIPTION_KEY esiste!!!!")
             headers["Ocp-Apim-Subscription-Key"] = os.getenv("APICFG_SUBSCRIPTION_KEY", default="")
-            print(f"#############SUBSCRIPTION_KEY {os.getenv("APICFG_SUBSCRIPTION_KEY", default="")}")
+            print(f"#############SUBSCRIPTION_KEY {os.getenv("APICFG_SUBSCRIPTION_KEY")}")
 
         url = apicfg_testing_support.get("base_path") + apicfg_testing_support.get("service")
         response = requests.post(url, data=query, headers=headers)
