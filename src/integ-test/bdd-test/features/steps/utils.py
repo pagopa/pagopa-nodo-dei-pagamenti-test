c38773457e70252540eca9b4270feb69f8be7998
Lucaaccenture <luca.acone@accenture.com>
2024-02-27 11:00:50 +0100

    upd url single thread

--- src/integ-test/bdd-test/features/steps/utils.py
+++ src/integ-test/bdd-test/features/steps/utils.py
@@ -376,7 +376,10 @@ def single_thread(context, soap_primitive, tipo):
         headers = {'X-Forwarded-For': '10.82.39.148', 'Host': 'api.dev.platform.pagopa.it:443'}
         if 'SUBSCRIPTION_KEY' in os.environ:
             headers = {'Ocp-Apim-Subscription-Key', os.getenv('SUBSCRIPTION_KEY') }
-        url_nodo = f"{get_rest_url_nodo(context, primitive)}"
+        if context.config.userdata.get("services").get("nodo-dei-pagamenti").get("rest_service") == "":
+            url_nodo = f"{get_rest_url_nodo(context, primitive)}/{primitive}"
+        else:
+            url_nodo = get_rest_url_nodo(context, primitive)
         print(url_nodo)
         soap_response = requests.get(url_nodo, headers=headers, verify=False)
         print("soap_response: ", soap_response.content)
@@ -391,7 +394,10 @@ def single_thread(context, soap_primitive, tipo):
             headers = {'Content-Type': 'application/xml', 'SOAPAction': primitive, 'Host': 'api.dev.platform.pagopa.it:443'}
             if 'SUBSCRIPTION_KEY' in os.environ:
                 headers['Ocp-Apim-Subscription-Key'] = os.getenv('SUBSCRIPTION_KEY')
-            url_nodo = get_soap_url_nodo(context, primitive)
+            if context.config.userdata.get("services").get("nodo-dei-pagamenti").get("soap_service").strip() == "":
+                url_nodo = f"{get_soap_url_nodo(context, primitive)}/{primitive}"
+            else:
+                url_nodo = get_soap_url_nodo(context, primitive)
             response = requests.post(url_nodo, body, headers=headers, verify=False)
         else:
             # headers = {'Content-Type': 'application/json', 'X-Forwarded-For': '10.82.39.148', 'Host': 'api.dev.platform.pagopa.it:443'}
@@ -425,9 +431,12 @@ def single_thread(context, soap_primitive, tipo):
             if 'SUBSCRIPTION_KEY' in os.environ:
                 headers['Ocp-Apim-Subscription-Key'] = os.getenv('SUBSCRIPTION_KEY')  
             if not already_xml:
-                body = json.loads(body)            
-            url_nodo = f"{get_rest_url_nodo(context, primitive)}"
-            response = requests.request(tipo, f"{url_nodo}", headers=headers, json=body, verify=False)
+                body = json.loads(body)
+            if context.config.userdata.get("services").get("nodo-dei-pagamenti").get("rest_service") == "":
+                url_nodo = f"{get_rest_url_nodo(context, primitive)}/{primitive}"
+            else:    
+                url_nodo = get_rest_url_nodo(context, primitive)
+            response = requests.request(tipo, url_nodo, headers=headers, json=body, verify=False)
 
         print("response: ", response.content)
         print(soap_primitive.split("_")[1] + "Response")
