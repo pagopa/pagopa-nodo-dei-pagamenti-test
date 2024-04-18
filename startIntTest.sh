2bf111766ecdff9e304ef16397f72a394452b017
Lucaaccenture <luca.acone@accenture.com>
2024-04-18 09:15:33 +0200

    url restored

--- startIntTest.sh
+++ startIntTest.sh
@@ -22,8 +22,8 @@ fi
 echo "replace config file $file START"
 
 #replace $file ".services.\"nodo-dei-pagamenti\".url"           "https://api.dev.platform.pagopa.it/nodo"
-#replace $file ".services.\"nodo-dei-pagamenti\".url"           "https://nodo-p-sit.tst-npc.sia.eu"
-replace $file ".services.\"nodo-dei-pagamenti\".url"           "https://test.nexi.ndp.pagopa.it/nodo-p-sit.tst-npc.sia.eu"
+replace $file ".services.\"nodo-dei-pagamenti\".url"           "https://nodo-p-sit.tst-npc.sia.eu"
+#replace $file ".services.\"nodo-dei-pagamenti\".url"           "https://test.nexi.ndp.pagopa.it/nodo-p-sit.tst-npc.sia.eu"
 replace $file ".services.\"nodo-dei-pagamenti\".healthcheck"   "/monitor/health"
 #replace $file ".services.\"nodo-dei-pagamenti\".healthcheck"   "/monitoring/v1/monitor/health"
 #replace $file ".services.\"nodo-dei-pagamenti\".soap_service"  ""
