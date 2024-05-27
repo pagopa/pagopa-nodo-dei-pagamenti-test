60e45b6e59b58ca695a63f2edaec3a39675d6b78
Lucaaccenture <luca.acone@accenture.com>
2024-05-27 18:51:39 +0200

    upd path from json

--- src/integ-test/bdd-test/features/steps/steps.py
+++ src/integ-test/bdd-test/features/steps/steps.py
@@ -787,8 +787,16 @@ def step_impl(context, primitive, type_table, filebody):
     try:
         # Legge la datatable e la mette in una dict
         dict_fields_values = utils.table_to_dict(context.table, type_table)
-        print(f"directory corrente!!!! -------------->>>>>>>{os.getcwd()}") 
-        file_json = open(f"/src/integ-test/bdd-test/resources/json/{filebody}.json")
+
+        file_json = ''
+        user_profile = getattr(context, "user_profile")
+        ####RUN DA LOCALE
+        if user_profile != None:
+            file_json = open(f"src/integ-test/bdd-test/resources/json/{filebody}.json")
+        ###RUN DA REMOTO
+        else:
+            file_json = open(f"/nodo/extracted/src/integ-test/bdd-test/resources/json/{filebody}.json")
+            
         data_json = json.load(file_json)
 
         payload = json.dumps(data_json)
