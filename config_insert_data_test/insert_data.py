e64707209370b2bcd618f8d1468eb402a6a6af45
Lucaaccenture <luca.acone@accenture.com>
2024-10-28 16:55:28 +0100

    add insert to canali and canali_nodo

--- config_insert_data_test/insert_data.py
+++ config_insert_data_test/insert_data.py
@@ -24,7 +24,7 @@ def get_db_connection(db_name, db_cfg, db_conf):
 
 
 
-# Funzione per eseguire l'inserimento dei dati
+# Funzione per eseguire l'inserimento si stazioni
 def insert_stazioni_data(dati_stazioni, dati_pa_stazione_pa):
     try:
         conn = get_db_connection("nodo_cfg", conf_postgres, db_config)
@@ -52,7 +52,7 @@ def insert_stazioni_data(dati_stazioni, dati_pa_stazione_pa):
         # Query per la tabella pa_stazione_pa
         query_pa_stazione_pa_template = """
         INSERT INTO nodo4_cfg.pa_stazione_pa (progressivo, fk_pa, fk_stazione, aux_digit, segregazione, quarto_modello, broadcast, pagamento_spontaneo)
-        VALUES($progressivo, $fk_pa, $fk_stazione, $aux_digit, $segregazione, $quarto_modello, $broadcast, $pagamento_spontaneo);
+        VALUES($progressivo, $fk_pa, $fk_stazione, $aux_digit, $segregazione, $quarto_modello, $broadcast, $pagamento_spontaneo)
         """
     
         i = 0
@@ -63,7 +63,7 @@ def insert_stazioni_data(dati_stazioni, dati_pa_stazione_pa):
             for keys,values in record.items():
                 query_stazioni = query_stazioni.replace(f"${keys}", f"'{str(values)}'" if values is not None else 'NULL', 1)
 
-        # Eseguire l'inserimento in batch per stazioni e salva lista obj_id
+        # Eseguire l'inserimento in batch per stazioni e replace obj_id
             cursor.execute(query_stazioni)
             obj_id = cursor.fetchone()[0]
             dati_pa_stazione_pa[i]['fk_stazione'] = obj_id
@@ -94,18 +94,94 @@ def insert_stazioni_data(dati_stazioni, dati_pa_stazione_pa):
         conf_postgres.closeConnection(conn)
 
 
+
+
+
+# Funzione per eseguire l'inserimento si stazioni
+def insert_canali_data(dati_canali, dati_canali_nodo):
+    try:
+        conn = get_db_connection("nodo_cfg", conf_postgres, db_config)
+        cursor = conn.cursor()
+        
+        # Query per la tabella canali nodo
+        query_canali_nodo_template = """
+        INSERT INTO nodo4_cfg.canali_nodo (redirect_ip, redirect_path, redirect_porta, redirect_query_string, 
+        modello_pagamento, multi_payment, ragione_sociale, rpt_rt_compliant, wsapi, redirect_protocollo, id_serv_plugin,
+        id_cluster, id_fesp_instance, lento, rt_push, agid_channel, on_us, carrello_carte, recovery, marca_bollo_digitale,
+        flag_io, versione_primitive, flag_travaso, flag_standin)
+        VALUES($redirect_ip, $redirect_path, $redirect_porta, $redirect_query_string, 
+        $modello_pagamento, $multi_payment, $ragione_sociale, $rpt_rt_compliant, $wsapi, $redirect_protocollo, $id_serv_plugin,
+        $id_cluster, $id_fesp_instance, $lento, $rt_push, $agid_channel, $on_us, $carrello_carte, $recovery, $marca_bollo_digitale,
+        $flag_io, $versione_primitive, $flag_travaso, $flag_standin) RETURNING obj_id
+        """
+
+
+        # Query per la tabella canali
+        query_canali_template = """
+        INSERT INTO nodo4_cfg.canali (id_canale, enabled, ip, password, porta, protocollo, servizio, descrizione, fk_intermediario_psp, proxy_enabled,
+        proxy_host, proxy_password, proxy_port, proxy_username, fk_canali_nodo, timeout, num_thread, use_new_fault_code, timeout_a, timeout_b, timeout_c,
+        servizio_nmp, target_host, target_port, target_path, target_host_nmp, target_port_nmp, target_path_nmp)
+        VALUES($id_canale, $enabled, $ip, $password, $porta, $protocollo, $servizio, $descrizione, $fk_intermediario_psp, $proxy_enabled,
+        $proxy_host, $proxy_password, $proxy_port, $proxy_username, $fk_canali_nodo, $timeout, $num_thread, $use_new_fault_code, $timeout_a, $timeout_b, $timeout_c,
+        $servizio_nmp, $target_host, $target_port, $target_path, $target_host_nmp, $target_port_nmp, $target_path_nmp)
+        """
+    
+        i = 0
+        #Prepara query canali_nodo
+        for record in dati_canali_nodo:
+            query_canali_nodo = query_canali_nodo_template
+            
+            for keys,values in record.items():
+                query_canali_nodo = query_canali_nodo.replace(f"${keys}", f"'{str(values)}'" if values is not None else 'NULL', 1)
+
+        # Eseguire l'inserimento in batch per canali nodo e replace obj_id
+            cursor.execute(query_canali_nodo)
+            obj_id = cursor.fetchone()[0]
+            dati_canali[i]['fk_canali_nodo'] = obj_id
+            i += 1
+
+        #prepara query canali
+        j = 0
+        for record in dati_canali:
+            query_canali = query_canali_template
+            
+            for keys,values in record.items():
+                query_canali = query_canali.replace(f"${keys}", f"'{str(values)}'" if values is not None else 'NULL', 1)
+            j += 1
+        # Eseguire l'inserimento in batch per canali
+            cursor.execute(query_canali)
+
+        # Salvare le modifiche
+        conn.commit()
+        print("Dati inseriti con successo!")
+
+        for canale in dati_canali:
+            print(f"Canale inserito -> fk_canali_nodo: {str(canale['fk_canali_nodo'])}")
+    except Exception as e:
+        conn.rollback()
+        print(f"Errore durante l'inserimento: {e}")
+    finally:
+        cursor.close()
+        conf_postgres.closeConnection(conn)
+
+
 def leggi_dati_da_file(file_path):
     try:
         dati_stazioni = []
-        dati_pa_stazione_pa = [] 
+        dati_pa_stazione_pa = []
+        dati_canali = []
+        dati_canali_nodo = [] 
         
         with open(file_path, 'r') as file:
             data = json.load(file)  # Carica il contenuto del file JSON in un dizionario o una lista
 
         count_stazioni = len(data['stazioni'])
         count_pa_stazione_pa = len(data['pa_stazione_pa'])
+        count_canali = len(data['canali'])
+        count_canali_nodo = len(data['canali_nodo'])
 
         assert count_stazioni == count_pa_stazione_pa, f"Numero di records per stazioni non corretto!!!!"
+        assert count_canali == count_canali_nodo, f"Numero di records per canali non corretto!!!!"
 
         for i in range(0, count_stazioni):
             dati_stazioni.append(data['stazioni'][i])
@@ -113,7 +189,13 @@ def leggi_dati_da_file(file_path):
         for i in range(0, count_pa_stazione_pa):    
             dati_pa_stazione_pa.append(data['pa_stazione_pa'][i])
 
-        return dati_stazioni,dati_pa_stazione_pa
+        for i in range(0, count_canali):
+            dati_canali.append(data['canali'][i])
+
+        for i in range(0, count_canali_nodo):    
+            dati_canali_nodo.append(data['canali_nodo'][i])
+
+        return dati_stazioni,dati_pa_stazione_pa,dati_canali,dati_canali_nodo
 
     except AssertionError as e:
         # Stampiamo il messaggio di errore dell'assert
@@ -124,5 +206,6 @@ def leggi_dati_da_file(file_path):
 # Esecuzione dello script
 if __name__ == '__main__':
     file_dati_stazioni = "C:\\Users\\luca.acone\\OneDrive - Accenture\\Desktop\\pagopanew\\pagopa-nodo-dei-pagamenti-test\\config_insert_data_test\\data_to_insert.json"
-    dati_stazioni,dati_pa_stazione_pa = leggi_dati_da_file(file_dati_stazioni)
+    dati_stazioni,dati_pa_stazione_pa,dati_canali,dati_canali_nodo = leggi_dati_da_file(file_dati_stazioni)
     insert_stazioni_data(dati_stazioni,dati_pa_stazione_pa)
+    insert_canali_data(dati_canali,dati_canali_nodo)
