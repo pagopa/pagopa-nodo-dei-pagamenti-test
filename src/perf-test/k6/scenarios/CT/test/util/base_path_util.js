c1227136a35115a68f3ab662fd032be4b0fdeeb6
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2025-01-13 22:28:13 +0100

    Update base_path_util.js

--- src/perf-test/k6/scenarios/CT/test/util/base_path_util.js
+++ src/perf-test/k6/scenarios/CT/test/util/base_path_util.js
@@ -64,7 +64,7 @@ export function getBasePath(baseUrl, primitive) {
 			"nodoInviaRPT": "/nodo-per-pa/v1",
 			"nodoInviaRT": "/nodo-per-psp/v1",
 			"nodoPAChiediInformativaPA": "/nodo-per-pa/v1",
-			"checkPosition": "/nodo-per-pm/v2/checkPosition",
+			"checkPosition": "/nodo-per-pm/v1/checkPosition",
 			"closePaymentV2": "/nodo-per-pm/v2/closepayment",
 
 			"nodoPerPMv1": "/nodo-per-pm/v1",
