edcd6f58400b5e3943694b5bb9c5d102f70d46a9
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2025-03-31 15:08:10 +0200

    set sleep to 1 before spo

--- src/perf-test/k6/scenarios/CT/test/TC06.01_new_new.js
+++ src/perf-test/k6/scenarios/CT/test/TC06.01_new_new.js
@@ -164,7 +164,7 @@ export function total() {
     let outcome = 'OK';
     res = closePaymentV2(baseRestUrl, rndAnagPsp, paymentToken, outcome, transactionId, pspTransactionId, importoTotaleDaVersare);
 
-    sleep(7);
+    sleep(1);
 
     res = sendPaymentOutcomeV2(baseSoapUrl, rndAnagPsp, paymentToken);
 }
