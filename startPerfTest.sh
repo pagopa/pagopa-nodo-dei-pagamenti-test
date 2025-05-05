651430be549eba9f3ac34d24ad55f0b1f899412a
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2025-05-05 17:06:13 +0200

    no apim

--- startPerfTest.sh
+++ startPerfTest.sh
@@ -15,7 +15,7 @@ executeScript()
 	test_step=$3
 	
 	#envTest="pagoPA_PERF_apim"
-	envTest="pagoPA_PERF_apim_pg"
+	envTest="pagoPA_PERF_apim"
 	
 	echo -----------------------------------------
 	echo *** Main K6 Perf Test Script ***
