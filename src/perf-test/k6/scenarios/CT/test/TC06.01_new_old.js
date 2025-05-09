bb88b4e7a677c15d1e6ad5fdddaa3aac5b8425cc
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2025-05-09 15:21:27 +0200

    fix sleep 5s

--- src/perf-test/k6/scenarios/CT/test/TC06.01_new_old.js
+++ src/perf-test/k6/scenarios/CT/test/TC06.01_new_old.js
@@ -177,7 +177,7 @@ export function total() {
     let outcome = 'OK';
     res = closePaymentV2(baseRestUrl, rndAnagPsp, paymentToken, outcome, transactionId, pspTransactionId, importoTotaleDaVersare);
 
-    sleep(5);3);
+    sleep(5);
 
     res = sendPaymentOutcomeV2(baseSoapUrl, rndAnagPsp, paymentToken);
 }
