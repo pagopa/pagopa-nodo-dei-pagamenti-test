fb55bf8fc91e8dd8edd98c8418b5accc1b37fe78
lorenzo1046 <lorenzo.francia@accenture.com>
2025-05-12 15:13:40 +0200

    fix rt push

--- src/integ-test/bdd-test/features/steps/steps.py
+++ src/integ-test/bdd-test/features/steps/steps.py
@@ -1275,6 +1275,100 @@ def step_impl(context, filebody, type_table, number):
         print("----->>>> Exception:", e)
         # Interrompiamo il test
         raise e
+    
+    
+@step('RPT{number:d} generation {filebody} with datatable {type_table}')
+def step_impl(context, number, filebody, type_table):
+    try:
+
+        assert context.table is not None, f"Datatable non inserita!!!"
+        # Legge la datatable per le where conditions e la mette in una dict
+        dict_fields_values = utils.table_to_dict(context.table, type_table)
+
+        file_path = ''
+        user_profile = None
+        try:
+            user_profile = getattr(context, "user_profile")
+        except AttributeError as e:
+            print(f"User Profile None: {e} ->>> remote run!")
+
+        dbRun = getattr(context, "dbRun")
+
+        if dbRun == "Postgres":
+            ###RUN SI DA LOCALE CHE DAREMOTO
+            file_path = f"src/integ-test/bdd-test/resources/xml/{filebody}.xml"
+        elif dbRun == "Oracle":       
+            ####RUN DA LOCALE
+            if user_profile != None:
+                # Specifica il percorso del tuo file XML da locale
+                file_path = f"src/integ-test/bdd-test/resources/xml/{filebody}.xml"
+            ###RUN DA REMOTO
+            else:      
+                # Specifica il percorso del tuo file XML da remoto
+                current_directory = os.getcwd()
+                
+                substring_current_directory = ""
+                
+                substring_current_directory = current_directory[:-2]
+
+                print("La directory corrente è:", current_directory)
+
+                file_path = f"{substring_current_directory}/nodo/extracted/src/integ-test/bdd-test/resources/xml/{filebody}.xml"             
+                
+                print("Il file path corrente è:", file_path)
+
+        # Leggi il contenuto del file XML come stringa
+        with open(file_path, 'r') as file:
+            payload = file.read()
+
+        #replace placeHolder with value by datatable
+        for fields, values in dict_fields_values.items():
+            for value in values:
+                payload = payload.replace(f"${fields}", value)
+
+        date = datetime.date.today().strftime("%Y-%m-%d")
+        timedate = date + datetime.datetime.now().strftime("T%H:%M:%S.%f")[:-3]
+
+        setattr(context, 'date', date)
+        setattr(context, 'timedate', timedate)
+        payload = utils.replace_local_variables(payload, context)
+        payload = utils.replace_context_variables(payload, context)
+
+        pa = context.config.userdata.get(
+            'global_configuration').get('creditor_institution_code')
+
+        if f'#iuv{number}#' in payload:
+            iuv = "IUV" + str(random.randint(0, 10000)) + "-" + \
+                datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S.%f")[:-3]
+            payload = payload.replace(f'#iuv{number}#', iuv)
+            setattr(context, f'{number}iuv', iuv)
+
+        if f"#ccp{number}#" in payload:
+            ccp = str(int(time.time() * 1000))
+            payload = payload.replace(f'#ccp{number}#', ccp)
+            setattr(context, f"{number}ccp", ccp)
+
+
+        payload = utils.replace_global_variables(payload, context)
+
+        setattr(context, f'rpt{number}', payload)
+        payload_b = bytes(payload, 'UTF-8')
+        payload_uni = b64.b64encode(payload_b)
+        payload = f"{payload_uni}".split("'")[1]
+
+        print(f"RPT{number} generato: ", payload)
+        setattr(context, f'rpt{number}Attachment', payload)
+
+    except AssertionError as e:
+        # Stampiamo il messaggio di errore dell'assert
+        print("----->>>> Assertion Error: ", e)
+        # Interrompiamo il test
+        raise AssertionError(str(e))
+    except Exception as e:
+        # Gestione di tutte le altre eccezioni
+        print("----->>>> Exception:", e)
+        # Interrompiamo il test
+        raise e
 
 
 
@@ -2079,10 +2173,10 @@ def step_impl(context, filebody, type_table, number):
             payload = payload.replace('#timedate#', timedate)
             setattr(context, 'timedate', timedate)
 
-        if "#ccp#" in payload:
-            ccp = str(utils.current_milli_time())
-            payload = payload.replace('#ccp#', ccp)
-            setattr(context, "ccp", ccp)
+        if f"#ccp{number}#" in payload:
+            ccp = str(int(time.time() * 1000))
+            payload = payload.replace(f'#ccp{number}#', ccp)
+            setattr(context, f"{number}ccp", ccp)
 
         setattr(context, f'rt{number}', payload)
 
@@ -2090,7 +2184,7 @@ def step_impl(context, filebody, type_table, number):
         payload_uni = b64.b64encode(payload_b)
         payload = f"{payload_uni}".split("'")[1]
 
-        print("RT generato: ", payload)
+        print(f"RT{number} generato: ", payload)
         setattr(context, f'rt{number}Attachment', payload)
 
     except AssertionError as e:
