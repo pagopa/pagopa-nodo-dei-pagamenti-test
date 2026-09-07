f814710e0c86a1fe57f9fa6fd793f22357b3699c
Tommaso Isgrò <tommaso.isgro@external.nexigroup.com>
2026-09-07 09:51:16 +0200

    PG 150 PERF Test, added new api in  06_01, 06_02 Flow

--- src/perf-test/k6/scenarios/CT/test/util/base_path_util.js
+++ src/perf-test/k6/scenarios/CT/test/util/base_path_util.js
@@ -34,44 +34,46 @@ export function getBasePath(baseUrl, primitive) {
 			"nodoPAChiediInformativaPA": "/webservices/input",
 			"checkPosition": "/checkPosition",
 			"closePaymentV2": "/v2/closepayment",
+				"paymentStatus": "/paymentstatus",
 
-			"nodoPerPMv1": "",
-			"nodoPerPMv2": ""
-		}
+				"nodoPerPMv1": "",
+				"nodoPerPMv2": ""
+			}
 	}
 	else {
-		primitiveMapping = {
-			"verificaBollettino": "/node-for-psp/v1",
-			"verifyPaymentNotice": "/node-for-psp/v1",
-			"activatePaymentNotice": "/node-for-psp/v1",
-			"activatePaymentNoticeV2": "/node-for-psp/v1",
-			"sendPaymentOutcome": "/node-for-psp/v1",
-			"sendPaymentOutcomeV2": "/node-for-psp/v1",
-			"activateIOPayment": "/node-for-io/v1",
-			"nodoVerificaRPT": "/nodo-per-psp/v1",
-			"nodoAttivaRPT": "/nodo-per-psp/v1",
-			"nodoInviaFlussoRendicontazione": "/nodo-per-psp/v1",
-			"nodoChiediElencoFlussiRendicontazione": "/nodo-per-pa/v1",
-			"nodoChiediFlussoRendicontazione": "/nodo-per-pa/v1",
-			"demandPaymentNotice": "/node-for-psp/v1",
-			"nodoChiediCatalogoServizi": "/nodo-per-psp-richiesta-avvisi/v1",
-			"nodoChiediCatalogoServiziV2": "/nodo-per-psp/v1",
-			"nodoChiediCopiaRT": "/nodo-per-pa/v1",
-			"nodoChiediInformativaPA": "/nodo-per-psp/v1",
-			"nodoChiediListaPendentiRPT": "/nodo-per-pa/v1",
-			"nodoChiediNumeroAvviso": "/nodo-per-psp-richiesta-avvisi/v1",
-			"nodoChiediStatoRPT": "/nodo-per-pa/v1",
-			"nodoChiediTemplateInformativaPSP": "/nodo-per-psp/v1",
-			"nodoInviaCarrelloRPT": "/nodo-per-pa/v1",
-			"nodoInviaRPT": "/nodo-per-pa/v1",
-			"nodoInviaRT": "/nodo-per-psp/v1",
-			"nodoPAChiediInformativaPA": "/nodo-per-pa/v1",
-			"checkPosition": "/nodo-per-pm/v1/checkPosition",
-			"closePaymentV2": "/nodo-per-pm/v2/closepayment",
+			primitiveMapping = {
+				"verificaBollettino": "/node-for-psp/v1",
+				"verifyPaymentNotice": "/node-for-psp/v1",
+				"activatePaymentNotice": "/node-for-psp/v1",
+				"activatePaymentNoticeV2": "/node-for-psp/v1",
+				"sendPaymentOutcome": "/node-for-psp/v1",
+				"sendPaymentOutcomeV2": "/node-for-psp/v1",
+				"activateIOPayment": "/node-for-io/v1",
+				"nodoVerificaRPT": "/nodo-per-psp/v1",
+				"nodoAttivaRPT": "/nodo-per-psp/v1",
+				"nodoInviaFlussoRendicontazione": "/nodo-per-psp/v1",
+				"nodoChiediElencoFlussiRendicontazione": "/nodo-per-pa/v1",
+				"nodoChiediFlussoRendicontazione": "/nodo-per-pa/v1",
+				"demandPaymentNotice": "/node-for-psp/v1",
+				"nodoChiediCatalogoServizi": "/nodo-per-psp-richiesta-avvisi/v1",
+				"nodoChiediCatalogoServiziV2": "/nodo-per-psp/v1",
+				"nodoChiediCopiaRT": "/nodo-per-pa/v1",
+				"nodoChiediInformativaPA": "/nodo-per-psp/v1",
+				"nodoChiediListaPendentiRPT": "/nodo-per-pa/v1",
+				"nodoChiediNumeroAvviso": "/nodo-per-psp-richiesta-avvisi/v1",
+				"nodoChiediStatoRPT": "/nodo-per-pa/v1",
+				"nodoChiediTemplateInformativaPSP": "/nodo-per-psp/v1",
+				"nodoInviaCarrelloRPT": "/nodo-per-pa/v1",
+				"nodoInviaRPT": "/nodo-per-pa/v1",
+				"nodoInviaRT": "/nodo-per-psp/v1",
+				"nodoPAChiediInformativaPA": "/nodo-per-pa/v1",
+				"checkPosition": "/nodo-per-pm/v1/checkPosition",
+				"closePaymentV2": "/nodo-per-pm/v2/closepayment",
+				"paymentStatus": "/nodo-per-ecommerce/v1/paymentstatus",
 
-			"nodoPerPMv1": "/nodo-per-pm/v1",
-			"nodoPerPMv2": "/nodo-per-pm/v2"
-		}
+				"nodoPerPMv1": "/nodo-per-pm/v1",
+				"nodoPerPMv2": "/nodo-per-pm/v2"
+			}
 	}
 	if (baseUrl.includes("nodo-dei-pagamenti-")) {
 		return baseUrl + primitiveMapping[primitive];
